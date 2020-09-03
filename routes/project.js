31eb0ec74483ffd328b39397410c2e205b5bb5a6
dencoSeca <leon.brown71@gmail.com>
2020-09-03 15:54:50 +0100

    add SHOW route for projects

--- routes/project.js
+++ routes/project.js
@@ -1,8 +1,9 @@
 const router = require('express').Router({ mergeParams: true })
 const Project = require('../models/Project')
 
+// CREATE
 router.post('/', async (req, res) => {
-  const projectExists = await Project.find({ name: req.body.name })
+  const projectExists = await Project.find({ author: req.body.author, name: req.body.name })
   if (projectExists.length > 0) {
     const newErr = new Error()
     newErr.message = 'Project with that name already exists'
@@ -11,12 +12,28 @@ router.post('/', async (req, res) => {
     Project.create(req.body, (err, project) => {
       if (err) {
         console.log(err)
-        err.message = 'Failed to create new project'
         res.send(err)
       }
       res.send(project)
     })
   }
+})
+
+// SHOW
+router.get('/', (req, res) => {
+  Project.findOne({ author: req.body.author, name: req.body.name }, (err, project) => {
+    if (err) {
+      console.log(err)
+      err.message = 'Something went wrong with mongoose'
+      res.send(err)
+    }
+    if (project == null) {
+      const newErr = new Error()
+      newErr.message = 'Cannot find project with that name'
+      res.send(newErr)
+    }
+    res.send(project)
+  })
 })
 
 module.exports = router
