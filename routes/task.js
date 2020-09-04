a0735a4a0e3bce0563c2d3ffad20ed7038b1200b
dencoSeca <leon.brown71@gmail.com>
2020-09-04 10:03:54 +0100

    add UPDATE route to task

--- routes/task.js
+++ routes/task.js
@@ -25,4 +25,23 @@ router.post('/create', (req, res) => {
   })
 })
 
+// UPDATE
+router.post('/update', (req, res) => {
+  Task.findByIdAndUpdate(req.body.id, req.body.task, (err, task) => {
+    if (err) {
+      console.log(err)
+      err.message = 'Mongoose threw an error trying to update a task'
+      res.send(err)
+    }
+    Task.findOne(task._id, (err, updatedTask) => {
+      if (err) {
+        console.log(err)
+        err.message = 'Mongoose threw an error trying to find a task'
+        res.send(err)
+      }
+      res.send(updatedTask)
+    })
+  })
+})
+
 module.exports = router
