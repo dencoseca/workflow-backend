608419f1cfa8302b1dd4022129ca1643810256f8
dencoSeca <leon.brown71@gmail.com>
2020-09-05 11:16:16 +0100

    changed login route to post

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const router = require('express').Router({ mergeParams: true })
 const User = require('../models/User')
 
 // CREATE
-router.post('/', async (req, res) => {
+router.post('/signup', async (req, res) => {
   const userExists = await User.find({ username: req.body.username })
   if (userExists.length > 0) {
     res.send({ message: 'User with that name already exists' })
@@ -15,12 +15,13 @@ router.post('/', async (req, res) => {
 })
 
 // SHOW
-router.get('/', (req, res) => {
-  User.findOne({ username: req.body.username }, (err, user) => {
+router.post('/login', (req, res) => {
+  console.log(req.body)
+  User.findOne({ username: req.body.username, password: req.body.password }, (err, user) => {
     if (err) {
       res.send({ message: 'Mongoose threw an error trying to find a user' })
     } else if (user == null) {
-      res.send({ message: 'Cannot find user with that name' })
+      res.send({ message: 'Username or password are incorrect' })
     } else {
       res.send(user)
     }
